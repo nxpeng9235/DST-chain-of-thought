9abe0318302461df88b1ae7ad1f6939e281c33d2
nxpeng9235 <pengningxin@163.com>
2022-08-27 10:37:20 +0000

    feat: change strategies in selecting key sentences

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,8 +1,9 @@
+import enum
 import os
 import sys
 import json
 from glob import glob
-from random import sample
+from tqdm import tqdm
 
 domain_desc_flag = True # To append domain descriptions or not 
 slot_desc_flag = True  # To append slot descriptions or not 
@@ -12,25 +13,31 @@ def preprocess(dial_json, schema, out, idx_out, excluded_domains, frame_idxs):
     dial_json_n = dial_json.split("/")[-1]
     dial_json = open(dial_json)
     dial_json = json.load(dial_json)
-    for dial_idx in range(len(dial_json)):
+    for dial_idx in tqdm(range(len(dial_json))):
         dial = dial_json[dial_idx]
-        uttr_list = []
         cur_dial = ""
-        for turn in dial["turns"]:
+
+        related_map = {}    # {key1: [(uttr1, value1), (uttr2, value2), ...], key2: [...]}
+
+        for turn_idx, turn in enumerate(dial["turns"]):
             speaker = " [" + turn["speaker"] + "] "
             uttr = turn["utterance"]
             cur_dial += speaker
             cur_dial += uttr
-            uttr_list.append(uttr)
 
             if turn["speaker"] == "USER":
                 active_slot_values = {}
                 for frame_idx in range(len(turn["frames"])):
                     frame = turn["frames"][frame_idx]
                     for key, values in frame["state"]["slot_values"].items():
-                        # if have multiple answers, randomly select one
-                        value = sample(values, 1)[0]
+                        # if have multiple answers, select the last one
+                        value = values[-1]
                         active_slot_values[key] = value
+                        # if the value first appears or different from the last one, then mark this as keysent
+                        if key not in related_map or value != related_map[key][-1][1]:
+                            tmp_list = related_map.get(key, [])
+                            tmp_list.append((uttr, value))
+                            related_map.update({key: tmp_list})
 
                 # iterate through each domain-slot pair in each user turn
                 for domain in schema:
@@ -41,16 +48,16 @@ def preprocess(dial_json, schema, out, idx_out, excluded_domains, frame_idxs):
                     slots = domain["slots"]
                     for slot in slots:
                         d_name, s_name = slot["name"].split("-")
-                        # related utterances for reasoning
-                        related_uttrs = []
                         # generate schema prompt w/ or w/o natural language descriptions
                         schema_prompt = ""
                         schema_prompt += " [domain] " + d_name
                         if domain_desc_flag:
                             schema_prompt += " " + domain["description"]
-                        schema_prompt += " [slot] " + s_name
+                        schema_prompt += " [slot] "
                         if slot_desc_flag:
-                            schema_prompt += " " + slot["description"]
+                            schema_prompt += slot["description"]
+                        else:
+                            schema_prompt += s_name
                         if PVs_flag:
                             # only append possible values if the slot is categorical
                             if slot["is_categorical"]:
@@ -59,12 +66,13 @@ def preprocess(dial_json, schema, out, idx_out, excluded_domains, frame_idxs):
                         
                         if slot["name"] in active_slot_values.keys():
                             target_value = active_slot_values[slot["name"]]
-                            for ut in uttr_list:
-                                if ut.lower().find(target_value) >= 0:
-                                    related_uttrs.append(ut)
+                            # related utterances for reasoning
+                            related_uttrs = related_map.get(slot["name"], [])
                             chain_of_thought = "Because "
-                            chain_of_thought += ", and ".join([f'"{ut}"'])
-                            target_value = f"{chain_of_thought}, the {d_name} {s_name} is {target_value}."
+                            chain_of_thought += ", and ".join([f'"{ut[0]}"' for ut in related_uttrs])
+                            # in MultiWOZ 2.2, directly use description of slot for prompting
+                            slot_prompt = slot['description'] if slot_desc_flag else f"{d_name} {s_name}"
+                            target_value = f"{chain_of_thought}, the {slot_prompt} is {target_value}."
                         else:
                             target_value = "NONE"
                         
@@ -76,7 +84,20 @@ def preprocess(dial_json, schema, out, idx_out, excluded_domains, frame_idxs):
                         idx_list = [ dial_json_n, str(dial_idx), turn["turn_id"], str(frame_idxs[d_name]), d_name, s_name ]
                         idx_out.write("|||".join(idx_list))
                         idx_out.write("\n")
-    return
+
+            elif turn["speaker"] == "SYSTEM":
+                for frame_idx in range(len(turn["frames"])):
+                    frame = turn["frames"][frame_idx]
+                    for system_slot in frame["slots"]:
+                        key, value = system_slot["slot"], system_slot["value"]
+                        # if system stated a value that impacted user's decision, add the uttr to related_map
+                        if key not in related_map or value != related_map[key][-1][1]:
+                            tmp_list = related_map.get(key, [])
+                            tmp_list.append((uttr, "##sys##"))
+                            related_map.update({key: tmp_list})
+
+            else:
+                pass
 
 
 def main():
--- train.sh
+++ train.sh
@@ -5,19 +5,23 @@ THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
 DATA_DIR=${THIS_DIR}/data-bin
 OUTPUT_DIR=${THIS_DIR}/outputs
 
-CUDA_VISIBLE_DEVICES=5 python3 src/train.py \
-    --model_name_or_path t5-small \
+TIME=`date +%Y%m%d%H%M%S`
+
+CUDA_VISIBLE_DEVICES=0 python3 src/train.py \
+    --model_name_or_path t5-large \
     --do_train \
     --do_predict \
     --train_file "$DATA_DIR/train.json" \
     --validation_file "$DATA_DIR/dev.json" \
     --test_file "$DATA_DIR/test.json" \
     --source_prefix "" \
-    --output_dir "$OUTPUT_DIR/t5small_mwoz2.2" \
-    --per_device_train_batch_size=16 \
-    --per_device_eval_batch_size=16 \
+    --output_dir "$OUTPUT_DIR/t5large_mwoz2.2/$TIME" \
+    --max_source_length=1024 \
+    --max_target_length=256 \
+    --per_device_train_batch_size=8 \
+    --per_device_eval_batch_size=8 \
     --num_train_epochs=3 \
     --predict_with_generate \
     --text_column="dialogue" \
     --summary_column="result" \
-    --save_steps=500000
+    --save_steps=300000
